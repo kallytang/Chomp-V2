5127a787bd3cd4d71750d0ff66e44c5106fdaad8
kallytang <tangka@oregonstate.edu>
2022-02-21 18:27:04 -0800

    Initial commit

---
+++ app/src/main/java/dev/kallytang/chomp/ChompApp.kt
@@ -0,0 +1,10 @@
+package dev.kallytang.chomp
+
+import androidx.multidex.MultiDexApplication
+
+class ChompApp:MultiDexApplication() {
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
